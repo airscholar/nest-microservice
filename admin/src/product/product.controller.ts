2fe49d02cb338749567c2f0f316c69f32fa205c1
Yusuf Ganiyu <airscholar@gmail.com>
2021-11-07 15:09:39 +0100

    Updated admin microservice app

--- admin/src/product/product.controller.ts
+++ admin/src/product/product.controller.ts
@@ -1,9 +1,11 @@
+import { ClientProxy } from '@nestjs/microservices';
 import { ProductService } from './product.service';
 import {
   Body,
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Patch,
   Post,
@@ -12,10 +14,14 @@ import { Product } from './product.entity';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    @Inject('PRODUCT_SERVICE') private readonly client: ClientProxy,
+  ) {}
 
   @Get()
   async getAllProducts() {
+    this.client.emit('hello', 'Hello there!');
     const products = await this.productService.getAllProducts();
     return { result: products };
   }
@@ -23,6 +29,9 @@ export class ProductController {
   @Post()
   async createProduct(@Body() product: Product) {
     const result = await this.productService.createProduct(product);
+
+    this.client.emit('product_created', product);
+
     return { result };
   }
 
@@ -40,12 +49,30 @@ export class ProductController {
   ) {
     const result = await this.productService.updateProductById(prodId, product);
 
+    this.client.emit('product_updated', { prodId, product: result });
+
     return { result };
+  }
+
+  @Get(':prodId/like')
+  async likeProduct(@Param('prodId') prodId: number) {
+    const product = await this.productService.getProductById(prodId);
+    product.likes++;
+    const updatedProduct = await this.productService.updateProductById(
+      prodId,
+      product,
+    );
+
+    console.log('Admin like', updatedProduct);
+    return { result: updatedProduct };
   }
 
   @Delete(':prodId')
   async deleteProductById(@Param('prodId') prodId: number) {
     const result = await this.productService.deleteProductById(prodId);
+
+    this.client.emit('product_deleted', prodId);
+
     return { result };
   }
 }
--- admin/src/product/product.module.ts
+++ admin/src/product/product.module.ts
@@ -1,3 +1,4 @@
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
@@ -5,7 +6,24 @@ import { ProductController } from './product.controller';
 import { Product } from './product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://peflsyyy:EYU_fMXRFP_lYw8aeMh3hpRHLyjBf71b@jaguar.rmq.cloudamqp.com/peflsyyy',
+          ],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+    TypeOrmModule.forFeature([Product]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
